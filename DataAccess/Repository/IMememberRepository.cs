d634ac83fc1d6899861e272d93ec312700be1d7c
V.T.TuongDi <vothituongduy20@gmail.com>
2022-06-24 09:59:46 +0700

    add repository interface

--- DataAccess/Repository/IMememberRepository.cs
+++ DataAccess/Repository/IMememberRepository.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BussinessObject;
 
 namespace DataAccess.Repository
 {
-    internal class IMememberRepository
+    public interface IMememberRepository
     {
+        public bool CreateMember(MemberObject newMember);
+
+        public bool DeleteMember(string id);
+
+        public bool UpdateMember(string id, MemberObject updatedMemberInfo);
+
+        public MemberObject GetMemberById(string id);
+
+        public List<MemberObject> GetAllMembers();
+
+        public List<MemberObject> SearchMemberByName(string name);
+
+        public List<MemberObject> SearchMemberById(string id);
+
+        public List<MemberObject> GetMemberByCity(string city);
+
+        public List<MemberObject> GetMemberByCountry(string country);
+
+        public MemberObject CheckLogin(string email, string password);
+
+        public bool ChangePassword(string id, string oldPassword, string newPassword);
+
+        public List<MemberObject> SearchMemberByIdAndName(string searchName, string searchId);
+
+        public List<MemberObject> GetMemberByCityAndCountry(string city, string country);
     }
 }
--- DataAccess/Repository/MemberRepository.cs
+++ DataAccess/Repository/MemberRepository.cs
@@ -3,10 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BussinessObject;
 
 namespace DataAccess.Repository
 {
-    internal class MemberRepository
+    public class MemberRepository : IMememberRepository
     {
+        public bool ChangePassword(string id, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MemberObject CheckLogin(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateMember(MemberObject newMember)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteMember(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MemberObject> GetAllMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MemberObject> GetMemberByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MemberObject> GetMemberByCityAndCountry(string city, string country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MemberObject> GetMemberByCountry(string country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MemberObject GetMemberById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MemberObject> SearchMemberById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MemberObject> SearchMemberByIdAndName(string searchName, string searchId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MemberObject> SearchMemberByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateMember(string id, MemberObject updatedMemberInfo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
