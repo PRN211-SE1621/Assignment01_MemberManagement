0340569c1725a32bed17d3af5172a17f7b0dcbe0
truongquangtan <truongquangtan2001@gmail.com>
2022-06-28 22:52:37 +0700

    resolve conflict

--- DataAccess/Repository/IMememberRepository.cs
+++ DataAccess/Repository/IMememberRepository.cs
@@ -1,139 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BussinessObject;
-using DataAccess.Context;
+﻿using BussinessObject;
 
 namespace DataAccess.Repository
 {
-    public class MemberRepository : IMememberRepository
+    public interface IMememberRepository
     {
-        public bool ChangePassword(string id, string oldPassword, string newPassword)
-        {
-            throw new NotImplementedException();
-        }
+        public bool CreateMember(MemberObject newMember);
 
-        public MemberObject CheckLogin(string email, string password)
-        {
-            throw new NotImplementedException();
-        }
+        public bool DeleteMember(string id);
 
-        public bool CreateMember(MemberObject newMember)
-        {
-            throw new NotImplementedException();
-        }
+        public bool UpdateMember(string id, MemberObject updatedMemberInfo);
 
-        public bool DeleteMember(string id)
-        {
-            throw new NotImplementedException();
-        }
+        public MemberObject GetMemberById(string id);
 
-        public IEnumerable<MemberObject> GetAllMembers() => MemberDBContext.Instance.GetMemberList();
+        public IEnumerable<MemberObject> GetAllMembers();
 
-        public List<MemberObject> GetMemberByCity(string city)
-        {
-            throw new NotImplementedException();
-        }
+        public List<MemberObject> SearchMemberByName(string name);
 
-        public List<MemberObject> GetMemberByCityAndCountry(string city, string country)
-        {
-            throw new NotImplementedException();
-        }
+        public List<MemberObject> SearchMemberById(string id);
 
-        public List<MemberObject> GetMemberByCountry(string country)
-        {
-            throw new NotImplementedException();
-        }
+        public List<MemberObject> GetMemberByCity(string city);
 
-        public MemberObject GetMemberById(string id)
-        {
-            MemberObject member = null;
-            try
-            {
-                int intId = int.Parse(id);
-                member = MemberDBContext.Instance.GetMemberByID(intId);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return member;
-        }
+        public List<MemberObject> GetMemberByCountry(string country);
 
-        public List<MemberObject> SearchMemberById(string id)
-        {
-            List<MemberObject> resultList = new List<MemberObject>();
-            try
-            {
-                
-                if(GetMemberById(id) != null)
-                    resultList.Add(GetMemberById(id));
-                int intId = int.Parse(id);
-                foreach (MemberObject member in MemberDBContext.Instance.GetMemberList())
-                {
-                    if (member.MemberID.ToString().Contains(id)&& !member.MemberID.ToString().Equals(id))
-                    {
-                        resultList.Add(member);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return resultList;
-        }
+        public MemberObject CheckLogin(string email, string password);
 
-        public List<MemberObject> SearchMemberByIdAndName(string searchName, string searchId)
-        {
-            List<MemberObject> resultList = new List<MemberObject>();
-            if (searchName == null || searchName.Equals(""))
-            {
-                if(searchId == null|| searchId.Equals("")){
-                    resultList.AddRange(GetAllMembers());
-                }
-                else
-                {
-                    resultList.AddRange(SearchMemberById(searchId));
-                }
-            }
-            else {
-                if (searchId == null || searchId.Equals(""))
-                {
-                    resultList.AddRange(SearchMemberByName(searchName);
-                }
-                else
-                {
-                    foreach(MemberObject member in SearchMemberById(searchId))
-                    {
-                        if (member.MemberID.ToString().Contains(searchId))
-                        {
-                            resultList.Add(member);
-                        }
-                    }
-                }
-            }
-            return resultList;
-        }
+        public bool ChangePassword(string id, string oldPassword, string newPassword);
 
-        public List<MemberObject> SearchMemberByName(string name)
-        {
-            List<MemberObject> resultList = new List<MemberObject>();
-                foreach (MemberObject member in MemberDBContext.Instance.GetMemberList())
-                {
-                    if (member.MemberName.Contains(name))
-                    {
-                        resultList.Add(member);
-                    }
-                }
-            return resultList;
-        }
+        public List<MemberObject> SearchMemberByIdAndName(string searchName, string searchId);
 
-        public bool UpdateMember(string id, MemberObject updatedMemberInfo)
-        {
-            throw new NotImplementedException();
-        }
+        public List<MemberObject> GetMemberByCityAndCountry(string city, string country);
     }
 }
--- DataAccess/Repository/MemberRepository.cs
+++ DataAccess/Repository/MemberRepository.cs
@@ -1,38 +1,136 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BussinessObject;
+﻿using BussinessObject;
+using DataAccess.Context;
 
 namespace DataAccess.Repository
 {
-    public interface IMememberRepository
+    public class MemberRepository : IMememberRepository
     {
-        public bool CreateMember(MemberObject newMember);
+        public bool ChangePassword(string id, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
 
-        public bool DeleteMember(string id);
+        public MemberObject CheckLogin(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
 
-        public bool UpdateMember(string id, MemberObject updatedMemberInfo);
+        public bool CreateMember(MemberObject newMember)
+        {
+            throw new NotImplementedException();
+        }
 
-        public MemberObject GetMemberById(string id);
+        public bool DeleteMember(string id)
+        {
+            throw new NotImplementedException();
+        }
 
-        public IEnumerable<MemberObject> GetAllMembers();
+        public IEnumerable<MemberObject> GetAllMembers() => MemberDBContext.Instance.GetMemberList();
 
-        public List<MemberObject> SearchMemberByName(string name);
+        public List<MemberObject> GetMemberByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
 
-        public List<MemberObject> SearchMemberById(string id);
+        public List<MemberObject> GetMemberByCityAndCountry(string city, string country)
+        {
+            throw new NotImplementedException();
+        }
 
-        public List<MemberObject> GetMemberByCity(string city);
+        public List<MemberObject> GetMemberByCountry(string country)
+        {
+            throw new NotImplementedException();
+        }
 
-        public List<MemberObject> GetMemberByCountry(string country);
+        public MemberObject GetMemberById(string id)
+        {
+            MemberObject member = null;
+            try
+            {
+                int intId = int.Parse(id);
+                member = MemberDBContext.Instance.GetMemberByID(intId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return member;
+        }
 
-        public MemberObject CheckLogin(string email, string password);
+        public List<MemberObject> SearchMemberById(string id)
+        {
+            List<MemberObject> resultList = new List<MemberObject>();
+            try
+            {
 
-        public bool ChangePassword(string id, string oldPassword, string newPassword);
+                if (GetMemberById(id) != null)
+                    resultList.Add(GetMemberById(id));
+                int intId = int.Parse(id);
+                foreach (MemberObject member in MemberDBContext.Instance.GetMemberList())
+                {
+                    if (member.MemberID.ToString().Contains(id) && !member.MemberID.ToString().Equals(id))
+                    {
+                        resultList.Add(member);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return resultList;
+        }
 
-        public List<MemberObject> SearchMemberByIdAndName(string searchName, string searchId);
+        public List<MemberObject> SearchMemberByIdAndName(string searchName, string searchId)
+        {
+            List<MemberObject> resultList = new List<MemberObject>();
+            if (searchName == null || searchName.Equals(""))
+            {
+                if (searchId == null || searchId.Equals(""))
+                {
+                    resultList.AddRange(GetAllMembers());
+                }
+                else
+                {
+                    resultList.AddRange(SearchMemberById(searchId));
+                }
+            }
+            else
+            {
+                if (searchId == null || searchId.Equals(""))
+                {
+                    resultList.AddRange(SearchMemberByName(searchName);
+                }
+                else
+                {
+                    foreach (MemberObject member in SearchMemberById(searchId))
+                    {
+                        if (member.MemberID.ToString().Contains(searchId))
+                        {
+                            resultList.Add(member);
+                        }
+                    }
+                }
+            }
+            return resultList;
+        }
 
-        public List<MemberObject> GetMemberByCityAndCountry(string city, string country);
+        public List<MemberObject> SearchMemberByName(string name)
+        {
+            List<MemberObject> resultList = new List<MemberObject>();
+            foreach (MemberObject member in MemberDBContext.Instance.GetMemberList())
+            {
+                if (member.MemberName.Contains(name))
+                {
+                    resultList.Add(member);
+                }
+            }
+            return resultList;
+        }
+
+        public bool UpdateMember(string id, MemberObject updatedMemberInfo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
