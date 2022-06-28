c2814ce8ef8f6dfbaf7e1f150f3c0bdf5a230439
trungtin1811 <se.nttin1811@gmail.com>
2022-06-28 23:24:59 +0700

    fixx

--- DataAccess/Context/MemberDBContext.cs
+++ DataAccess/Context/MemberDBContext.cs
@@ -1,12 +1,7 @@
 ﻿using AutomobileLibrary.DataAccess;
 using BussinessObject;
 using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Context
 {
